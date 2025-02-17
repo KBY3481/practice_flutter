db5e7a1a273a4fb10aabb8c7ba76a2b0e3c66573
KBY3481 <robotkoo@gmail.com>
2025-02-17 17:41:30 +0900

    ddd

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:dronefield_app/common/component/custom_text_form_field.dart';
 import 'package:dronefield_app/user/view/login_screen.dart';
 import 'package:flutter/material.dart';
+import '../common/const/colors.dart';
 
 void main() {
   runApp(
@@ -15,6 +16,7 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        focusColor: primaryColor,
         fontFamily: 'NotoSans',
       ),
       debugShowCheckedModeBanner: false,
